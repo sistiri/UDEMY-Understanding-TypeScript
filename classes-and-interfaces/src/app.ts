400e67c3cf2f155944f03ecb1808e25540929e5d
sistiri <sistiri@yahoo.com>
2022-02-22 14:14:37 +0100

    Overriding Properties in Extended Classes & The "protected" Modifier

--- classes-and-interfaces/src/app.ts
+++ classes-and-interfaces/src/app.ts
@@ -1,7 +1,8 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+//   private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -35,6 +36,12 @@ class AccountingDepartment extends Department {
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
   }
+  addEmployee(name: string) {
+    if(name === 'Max') {
+        return
+    }
+    this.employees.push(name)
+  }
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -61,5 +68,7 @@ it.printEmployeeInformation();
 
 const accounting = new AccountingDepartment("d2", []);
 accounting.addReport("Something went wrong...");
+accounting.addEmployee('Max')
+accounting.addEmployee('Maxim')
 accounting.printReports();
 console.log(accounting)
