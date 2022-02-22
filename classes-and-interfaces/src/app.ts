fff72a3a336dce839c1b03f9d3a3fd3072dd04a0
sistiri <sistiri@yahoo.com>
2022-02-22 12:30:59 +0100

    Shorthand Initialization of Class via Constructor Function

--- classes-and-interfaces/src/app.ts
+++ classes-and-interfaces/src/app.ts
@@ -1,32 +1,35 @@
 class Department {
-  public name: string;
+  // private id: string;
+  // private name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
   }
 
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
   addEmployee(employee: string) {
     this.employees.push(employee);
   }
 
   printEmployeeInformation() {
-      console.log(this.employees.length)
-      console.log(this.employees)
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department('d1', "Accounting");
 
-accounting.addEmployee('Max')
-accounting.addEmployee('Manu')
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
 
 // accounting.employees[2] = "Anna"  // NOT WORKING because PRIVATE property
 
 accounting.describe();
+accounting.name = 'NEW NAME'
 accounting.printEmployeeInformation();
 
 // const accountingCopy = { name: "MUST HAVE", describe: accounting.describe };
