d70302450d1b9ea68f23005182767dcdb811d023
sistiri <sistiri@yahoo.com>
2022-02-23 10:30:11 +0100

    Discriminated Unions

--- typescript-advanced-types/src/app.ts
+++ typescript-advanced-types/src/app.ts
@@ -66,14 +66,41 @@ type Vehicle = Car | Truck;
 const v1 = new Car();
 const v2 = new Truck();
 
-
-function useVehicle(vehicle:Vehicle) {
-    vehicle.drive()
-    if(vehicle instanceof Truck) {
-
-        vehicle.loadCargo(1000)
-    }
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
 }
 
-useVehicle(v1)
-useVehicle(v2)
+useVehicle(v1);
+useVehicle(v2);
+
+// DISCRIMINATED UNIONS
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log("Moving at speed: " + speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed:10})
