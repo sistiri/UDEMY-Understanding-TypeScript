02b4f0905570ebaff45fe0ac981bb06f1b457d96
sistiri <sistiri@yahoo.com>
2022-02-23 14:28:27 +0100

    Creating a Generic Function

--- typescript-generics/src/app.ts
+++ typescript-generics/src/app.ts
@@ -1,12 +1,29 @@
-const names: Array<string> = ["Max", "Manuel"]; // string[]
-names[0].split(" ");
+// const names: Array<string> = ["Max", "Manuel"]; // string[]
+// names[0].split(" ");
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 2000);
-});
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
 
-promise.then((data) => {
-//   data.split(" ");
-});
+// promise.then((data) => {
+// //   data.split(" ");
+// });
+
+
+function mergeOld(objA: object, objB: object) {
+    return Object.assign(objA, objB)
+}
+
+function mergeNew<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB)
+}
+
+console.log(mergeOld({name: 'Max'}, {age: 30}))
+
+const mergedObjOld = mergeOld({name: 'Max'}, {age: 30})
+// console.log(mergedObjOld.age);
+const mergedObjNew = mergeNew({name: 'Max', hobbies: ['Sports']}, {age: 30})
+console.log(mergedObjNew)
+console.log(mergedObjNew.age);
