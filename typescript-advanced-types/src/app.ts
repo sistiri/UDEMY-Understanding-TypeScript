fb78b7e4ebf7ddef0789fa319cf30fe34868e5c9
sistiri <sistiri@yahoo.com>
2022-02-23 09:42:04 +0100

    Intersection Types

--- typescript-advanced-types/src/app.ts
+++ typescript-advanced-types/src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+type Admin = {
+    name: string;
+    priviliges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date
+}
+
+type ElevatedEmnployee = Admin & Employee;
+
+const e1: ElevatedEmnployee ={
+    name: 'Max',
+    priviliges: ['create-server'],
+    startDate: new Date()
+}
+
+type Combinable = string | number
+type Numeric = number | boolean
+
+type Universal = Combinable & Numeric
