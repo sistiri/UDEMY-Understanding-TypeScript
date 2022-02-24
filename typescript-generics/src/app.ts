f3038d574487a044e3ab16ef0ddcd528bcdba3a1
sistiri <sistiri@yahoo.com>
2022-02-24 09:01:57 +0100

    Working with Constraints

--- typescript-generics/src/app.ts
+++ typescript-generics/src/app.ts
@@ -16,14 +16,14 @@ function mergeOld(objA: object, objB: object) {
     return Object.assign(objA, objB)
 }
 
-function mergeNew<T, U>(objA: T, objB: U) {
-    return Object.assign(objA, objB)
-}
-
 console.log(mergeOld({name: 'Max'}, {age: 30}))
-
 const mergedObjOld = mergeOld({name: 'Max'}, {age: 30})
 // console.log(mergedObjOld.age);
-const mergedObjNew = mergeNew({name: 'Max', hobbies: ['Sports']}, {age: 30})
+
+function mergeNew<T extends object, U extends object >(objA: T, objB: U) {
+    return Object.assign(objA, objB)
+}
+const mergedObjNew = mergeNew({name: 'Max', hobbies: ['Sports']}, {age: 30} )
 console.log(mergedObjNew)
 console.log(mergedObjNew.age);
+console.log(mergedObjNew.name);
