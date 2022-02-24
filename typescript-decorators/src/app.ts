3d7ab2f8291e2a95d5faac4cd076f11d08ced765
sistiri <sistiri@yahoo.com>
2022-02-24 11:40:20 +0100

    Property Decorators

--- typescript-decorators/src/app.ts
+++ typescript-decorators/src/app.ts
@@ -1,32 +1,64 @@
 function Logger(logString: string) {
-    console.log('LOGGER FACTORY')
-    return function(constructor: Function){
-        console.log(logString)
-        console.log(constructor)
-    }
+  console.log("LOGGER FACTORY");
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
 function WithTemplate(template: string, hookId: string) {
-    console.log('TEMPLATE FACTORY')
-    return function(constructor: any){
-        console.log('Rendering template')
-        const hookEl = document.getElementById(hookId)
-        const p = new constructor()
-        if(hookEl){
-            hookEl.innerHTML = template
-            hookEl.querySelector('h1')!.textContent = p.name
-        }
+  console.log("TEMPLATE FACTORY");
+  return function (constructor: any) {
+    console.log("Rendering template");
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
     }
+  };
 }
 
-@Logger('LOGGING - PERSON')
-@WithTemplate('<h1></h1>', 'app')
+@Logger("LOGGING - PERSON")
+@WithTemplate("<h1></h1>", "app")
 class Person {
-    name = 'Max';
-    constructor() {
-        console.log('Creating person object')
-    }
+  name = "Max";
+  constructor() {
+    console.log("Creating person object");
+  }
 }
 
-const pers = new Person()
-console.log(pers)
+const pers = new Person();
+console.log(pers);
+
+// PROPERTY DECORATORS
+
+console.log('----- Property Decorators -----')
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithtax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
