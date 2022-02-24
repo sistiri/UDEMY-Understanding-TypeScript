97f02b578836633f833fdd3941345b38fa4032e6
sistiri <sistiri@yahoo.com>
2022-02-24 09:14:16 +0100

    Another Generic Function

--- typescript-generics/src/app.ts
+++ typescript-generics/src/app.ts
@@ -27,3 +27,25 @@ const mergedObjNew = mergeNew({name: 'Max', hobbies: ['Sports']}, {age: 30} )
 console.log(mergedObjNew)
 console.log(mergedObjNew.age);
 console.log(mergedObjNew.name);
+
+// ANOTHER GENERIC FUNCTION
+
+interface Lengthy {
+    length: number
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+   let descriptionText = 'Got no value!'
+    if(element.length === 1) {
+        descriptionText = 'Got 1 element.'
+    } else if (element.length > 1) {
+       descriptionText = 'Got ' + element.length + ' elements'
+    }
+
+
+    return [element, descriptionText]
+}
+
+console.log(countAndDescribe('Hi there!'))
+console.log(countAndDescribe(['Sports', 'Cooking']))
+
