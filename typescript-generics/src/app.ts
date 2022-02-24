b3464a601cbfff81f87c5d00b5c7db783368f847
sistiri <sistiri@yahoo.com>
2022-02-24 10:01:04 +0100

    Generic Classes

--- typescript-generics/src/app.ts
+++ typescript-generics/src/app.ts
@@ -57,3 +57,46 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 
 extractAndConvert({name: 'Max'}, 'name')
 
+
+// GENERIC CLASSES
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item)
+    }
+
+    removeItem(item: T) {
+        if(this.data.indexOf(item) === -1){
+            return
+        }
+        this.data.splice(this.data.indexOf(item), 1)
+    }
+
+    getItems() {
+        return [...this.data]
+    }
+}
+
+const textStorage = new DataStorage<string>()
+textStorage.addItem('Max')
+textStorage.addItem('Manu')
+textStorage.removeItem('Manu')
+console.log(textStorage.getItems())
+
+const numberStorage = new DataStorage<number>()
+numberStorage.addItem(10)
+numberStorage.addItem(20)
+numberStorage.removeItem(20)
+console.log(numberStorage.getItems())
+
+
+// not working with objects as it works with primitive values
+// const objectStorage = new DataStorage<object>()
+// const maxObj = {name: 'Max'}
+// objectStorage.addItem(maxObj)
+// objectStorage.addItem({name: 'Manu'})
+// objectStorage.removeItem({name: 'Max'})
+// console.log(objectStorage.getItems())
+
