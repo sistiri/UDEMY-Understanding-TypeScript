a97f0e31b312a7139e38a61f4a5cffa32e681f35
sistiri <sistiri@yahoo.com>
2022-02-24 11:14:08 +0100

    Building More Useful Decorators #1

--- typescript-decorators/src/app.ts
+++ typescript-decorators/src/app.ts
@@ -5,8 +5,17 @@ function Logger(logString: string) {
     }
 }
 
+function WithTemplate(template: string, hookId: string) {
+    return function(_: Function){
+        const hookEl = document.getElementById(hookId)
+        if(hookEl){
+            hookEl.innerHTML = template
+        }
+    }
+}
 
-@Logger('LOGGING - PERSON')
+// @Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = 'Max';
 
