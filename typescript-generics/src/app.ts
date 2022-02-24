61cc69f3c96ac71237e53285d0ebe50a12e7beae
sistiri <sistiri@yahoo.com>
2022-02-24 09:23:57 +0100

    The "keyof" Constraint

--- typescript-generics/src/app.ts
+++ typescript-generics/src/app.ts
@@ -49,3 +49,11 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 console.log(countAndDescribe('Hi there!'))
 console.log(countAndDescribe(['Sports', 'Cooking']))
 
+// THE "KEYOF" CONSTRAINT
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return obj[key];
+}
+
+extractAndConvert({name: 'Max'}, 'name')
+
