d1c40a7e51e8fcb6a3f5ab7c56d48a7b6183f89d
sistiri <sistiri@yahoo.com>
2022-03-01 15:18:06 +0100

    Finishing the Setup & Adding webpack-dev-server

--- typescript-drag-and-drop-project/src/app.ts
+++ typescript-drag-and-drop-project/src/app.ts
@@ -1,6 +1,7 @@
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
 
+// console.log('HELLO')
   new ProjectInput();
   new ProjectList("active");
   new ProjectList("finished");
--- typescript-drag-and-drop-project/webpack.config.js
+++ typescript-drag-and-drop-project/webpack.config.js
@@ -1,22 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/app.ts',
+  mode: "development",
+  entry: "./src/app.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "dist",
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js']
-  }
+    extensions: [".ts", ".js"],
+  },
 };
