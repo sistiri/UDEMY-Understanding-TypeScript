bf8525f582fbabce3e11e05d2d7386d71d3ebb53
sistiri <sistiri@yahoo.com>
2022-03-01 09:48:49 +0100

    Redifining Namespace Imports

--- typescript-drag-and-drop-project/src/components/project-item.ts
+++ typescript-drag-and-drop-project/src/components/project-item.ts
@@ -1,4 +1,7 @@
 /// <reference path='base-component.ts' />
+/// <reference path='../models/drag-drop.ts' />
+/// <reference path='../models/project.ts' />
+/// <reference path='../decorators/autobind.ts' />
 
 namespace App {
   // ProjectItem Class
