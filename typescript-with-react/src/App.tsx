db9908fc27d746b3afc28bbf93f888fdea35ef07
sistiri <sistiri@yahoo.com>
2022-03-02 16:16:58 +0100

    Working with State & Types => useState()

--- typescript-with-react/src/App.tsx
+++ typescript-with-react/src/App.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import TodoList from "./components/TodoList";
+import { Todo } from "./models/todo.model";
 
 // import './App.css';
 
 // function App() {
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "Finish the course" }];
+  // const todos = [{ id: "t1", text: "Finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text)
-  }
+    // setTodos([...todos, {id: Math.random().toString(), text: text}]) // NOT GOOD ENOUGH
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
 
   return (
     <div className="App">
