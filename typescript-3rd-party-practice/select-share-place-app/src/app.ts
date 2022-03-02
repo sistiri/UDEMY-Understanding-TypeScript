c2d7ddcfdf15ec6f01bbd5d3a6a4865ce2565699
sistiri <sistiri@yahoo.com>
2022-03-02 12:03:43 +0100

    Rendering a Map with Google Maps (incl. Types!)

--- typescript-3rd-party-practice/select-share-place-app/src/app.ts
+++ typescript-3rd-party-practice/select-share-place-app/src/app.ts
@@ -6,6 +6,8 @@ const addressInput = document.getElementById("address")! as HTMLInputElement;
 const GOOGLE_API_KEY = "";
 // https://developers.google.com/maps/documentation/geocoding/start#geocoding-request-and-response-latitudelongitude-lookup
 
+// declare var google: any;
+
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
   status: "OK" | "ZERO_RESULTS";
@@ -30,6 +32,15 @@ function searchAddressHandler(event: Event) {
         throw new Error("Could not fetch location!");
       }
       const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: coordinates,
+        zoom: 16,
+      });
+
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
     })
     .catch((err) => {
       alert(err.message);
