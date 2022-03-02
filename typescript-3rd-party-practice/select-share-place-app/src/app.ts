7f59774ea722e11fc259e367c0a38b0a62d2f07d
sistiri <sistiri@yahoo.com>
2022-03-02 11:18:58 +0100

    Setting Up a Google API Key

--- typescript-3rd-party-practice/select-share-place-app/src/app.ts
+++ typescript-3rd-party-practice/select-share-place-app/src/app.ts
@@ -1,6 +1,9 @@
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById('address')! as HTMLInputElement
 
+const GOOGLE_API_KEY = ''
+// https://developers.google.com/maps/documentation/geocoding/start#geocoding-request-and-response-latitudelongitude-lookup
+
 function searchAddressHandler(event: Event) {
     event.preventDefault()
     const enteredAdrress = addressInput.value
