dea56a08bb347adef5fe3ca62a72efcbd90dd9d9
sistiri <sistiri@yahoo.com>
2022-03-02 12:16:48 +0100

    Working with Maps without a Credit Card => OpenLayers

--- typescript-3rd-party-practice/select-share-place-app/src/app.ts
+++ typescript-3rd-party-practice/select-share-place-app/src/app.ts
@@ -1,51 +1,21 @@
-import axios from "axios";
-
-const form = document.querySelector("form")!;
-const addressInput = document.getElementById("address")! as HTMLInputElement;
-
-const GOOGLE_API_KEY = "";
-// https://developers.google.com/maps/documentation/geocoding/start#geocoding-request-and-response-latitudelongitude-lookup
-
-// declare var google: any;
-
-type GoogleGeocodingResponse = {
-  results: { geometry: { location: { lat: number; lng: number } } }[];
-  status: "OK" | "ZERO_RESULTS";
-// https://developers.google.com/maps/documentation/geocoding/requests-geocoding#StatusCodes
-};
-
+declare var ol: any;
+ 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
-  const enteredAddress = addressInput.value;
-
-  // send this to Google's API!
-
-  axios
-    .get<GoogleGeocodingResponse>(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-        enteredAddress
-      )}&key=${GOOGLE_API_KEY}`
-    )
-    .then((response) => {
-        console.log(response)
-      if (response.data.status !== "OK") {
-        throw new Error("Could not fetch location!");
-      }
-      const coordinates = response.data.results[0].geometry.location;
-      const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center: coordinates,
-        zoom: 16,
-      });
-
-      new google.maps.Marker({
-        position: coordinates,
-        map: map,
-      });
+ 
+  const coordinates = {lat: 40.41, lng: -73.99}; // Can't fetch coordinates from Google API, use dummy ones
+ 
+  document.getElementById('map')!.innerHTML = ''; // clear <p> from <div id="map">
+  new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 16
     })
-    .catch((err) => {
-      alert(err.message);
-      console.log(err);
-    });
+  });
 }
-
-form.addEventListener("submit", searchAddressHandler);
