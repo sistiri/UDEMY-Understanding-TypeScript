1e8c88b1f1d3adfcc103c3cc6b29b976dd005932
sistiri <sistiri@yahoo.com>
2022-03-02 16:05:28 +0100

    Cross-Component Communication

--- typescript-with-react/src/App.tsx
+++ typescript-with-react/src/App.tsx
@@ -7,9 +7,14 @@ import TodoList from "./components/TodoList";
 // function App() {
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course" }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text)
+  }
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
